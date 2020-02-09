443dd49d3646811c93120034499e9f770d30911c
petadox <jesusgpfib@gmail.com>
2020-02-09 14:56:14 +0100

    Adding first version of Aquarium Management System

---
+++ src/AMS.kt
@@ -0,0 +1,41 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    println("Hello, ${args[0]}")
+    feedTheFish()
+}
+
+fun shouldChangeWater(
+        day: String,
+        temperature: Int = 22,
+        dirty: Int = 20) : Boolean {
+    return true
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println ("Today is $day and the fish eat $food")
+
+    if (shouldChangeWater(day)) {
+        println("Change the water today")
+    }
+}
+
+fun randomDay() : String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(7)]
+}
+
+fun fishFood (day : String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Tuesday" -> "pellets"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Saturday" -> "lettuce"
+        "Sunday" -> "plankton"
+        else -> "fasting"
+    }
+}
